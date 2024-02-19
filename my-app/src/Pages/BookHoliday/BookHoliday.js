db6afb7850323fc19bd1f541f96e9eb68e964dcd
Deepak Kumar <dkkr30907@gmail.com>
2024-02-19 14:49:15 +0530

    Tajj_Holidays

---
+++ my-app/src/Pages/BookHoliday/BookHoliday.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../../Component/Home_Pg/Footer/Footer';
+import Booking from '../../Component/BookHoliday/Booking';
+
+function BookHoliday(props) {
+    return (
+        <div>
+            <Booking></Booking>
+            <Footer></Footer>
+        </div>
+    );
+}
+
+export default BookHoliday;
